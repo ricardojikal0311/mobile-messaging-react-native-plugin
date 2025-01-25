f8c63fd11d586811a24e29f04967db3cab7c5fd6
ricardojikal0311 <15992876+ricardojikal0311@users.noreply.github.com>
2025-01-25 21:12:30 +0000

    sync with iOS native

--- restore.sh
+++ restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2024-07-01"  # YYYY-MM-DD
-END_DATE="2024-11-18"    # YYYY-MM-DD
+START_DATE="2025-01-01"  # YYYY-MM-DD
+END_DATE="2025-05-18"    # YYYY-MM-DD
 
 # Min and max commits per selected day
 MIN_COMMITS=1
